d27b99d694a6f2c1aeaea8dc004ca0c93900e096
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-24 22:06:04 +0530

    rust variables

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\abhij\projects\rust\variables\target\debug\deps\variables.d: src\main.rs
+
+C:\Users\abhij\projects\rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\abhij\projects\rust\variables\target\debug\variables.exe: C:\Users\abhij\projects\rust\variables\src\main.rs
