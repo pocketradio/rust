4c3d8909916a6eb245dad338ee8a6cdd5931045c
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-01-07 16:57:49 +0530

    syntax errors fix

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,13 +7,12 @@ fn main() {
 
     let x = 5;
     make_copy(x); // x does nOT move into the function because its an integer ( fixed size ) and its stored directly on the stack.
-                  // the function instead gets a COPY.
+    // the function instead gets a COPY.
 
     let s2 = String::from("hiiii");
 
-    let s3 = take_and_give_back(s2) // so s2 is invalidated here since it moves into the function. then the function returns a value which 
-    // moves into s3. 
-
+    let s3 = take_and_give_back(s2); // so s2 is invalidated here since it moves into the function. then the function returns a value which 
+    // moves into s3.
 } // x goes out of scope here. s3 goes out of scope. s2 was moved, so nothing happens. 
 
 fn take_ownership(s2: String) {
@@ -24,14 +23,9 @@ fn make_copy(int1: i32) {
     println!("integer is {int1}");
 } // int1 goes out of scope. removed from stack.
 
-
-fn take_and_give_back(str1 : String){
-
+fn take_and_give_back(str1: String) -> String {
     str1 // returns back to fn call
 }
-
-
-
 
 /*
 
